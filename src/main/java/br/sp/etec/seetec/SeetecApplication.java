7427a960b6d0d899198791361d165bdcbf8566f8
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2020-05-04 01:31:12 -0300

    commit incial do projeto seetec

---
+++ src/main/java/br/sp/etec/seetec/SeetecApplication.java
@@ -0,0 +1,13 @@
+package br.sp.etec.seetec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeetecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeetecApplication.class, args);
+	}
+
+}
